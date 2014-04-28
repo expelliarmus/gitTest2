7a338bc192c754e15b63df5047019f4a3b179fdb
pandaharry <pcbeta@pcbetadeMac.local>
2014-04-28 12:55:08 +0800

    Initial Commit

---
+++ 长理吃货通Tests/_____Tests.m
@@ -0,0 +1,34 @@
+//
+//  _____Tests.m
+//  长理吃货通Tests
+//
+//  Created by pandaharry on 14-4-28.
+//  Copyright (c) 2014年 pandaharry. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _____Tests : XCTestCase
+
+@end
+
+@implementation _____Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
